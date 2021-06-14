a3d8fea44377e189ee3145fc4b215d02bc1bff13
Mark Coleman <mark.coleman@iress.com>
2021-06-14 11:34:48 +0100

    Default API implementation

---
+++ src/WeatherService.Api/Controllers/WeatherForecastController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System.Collections.Generic;
+
+namespace WeatherService.Api.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class WeatherForecastController : ControllerBase
+    {
+        private readonly ILogger<WeatherForecastController> _logger;
+
+        public WeatherForecastController(ILogger<WeatherForecastController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public IEnumerable<WeatherForecast> Get()
+        {
+            var logic = new WeatherLogic();
+            return logic.GetWeather();
+        }
+    }
+}
---
+++ src/WeatherService.Api/WeatherLogic.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections.Generic;
+
+namespace WeatherService.Api
+{
+    public class WeatherLogic
+    {
+        public IEnumerable<WeatherForecast> GetWeather()
+        {
+            // Typically this would have validation, mapping between models and entities etc
+
+            var dataStore = new WeatherDataStore();
+            return dataStore.GetWeather();
+        }
+
+    }
+}
